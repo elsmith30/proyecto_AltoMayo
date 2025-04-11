edd5ec3eaa8ce52bd9cbf91c900c9cdc6f1e6a12
elsmith30 <smithgalvezburga@gmail.com>
2025-04-11 11:50:56 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 function mostrarDato() {
   document.getElementById("dato").textContent = "¿Sabías que Moyobamba tiene más de 3,000 especies de orquídeas registradas?";
 }
+document.querySelectorAll('.cuadro img').forEach((img, index) => {
+  img.style.position = 'relative';
+  let direction = 1;
+  let distance = 1;
+  
+  setInterval(() => {
+    let currentTop = parseFloat(img.style.top) || 0;
+    if (currentTop >= 5 || currentTop <= -5) direction *= -1;
+    img.style.top = (currentTop + distance * direction) + 'px';
+  }, 50 + index * 20);
+});
